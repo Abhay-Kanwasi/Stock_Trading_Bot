06a601c451f6b83f3b29af8ada14794f327a038f
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2024-12-12 15:00:43 +0530

    changes done

--- src/helpers/clients/_alpha_vantage.py
+++ src/helpers/clients/_alpha_vantage.py
@@ -8,13 +8,17 @@ def main():
         "api_key": ALPHAVANTAGE_API_KEY,
         "ticker": "AAPL",
         "function": "TIME_SERIES_INTRADAY",
+        "minute": 1
     }
 
     # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-    url = 'https://www.alphavantage.co/query?function={function}&symbol={ticker}&interval=5min&apikey={api_key}'.format(**params)
+    url = 'https://www.alphavantage.co/query?function={function}&symbol={ticker}&interval={minute}min&apikey={api_key}'.format(**params)
     r = requests.get(url)
     data = r.json()
-
+    dataset_key = [x for x in list(data.keys()) if not x.lower() == "meta data"][0]
+    results = data[dataset_key]
+    results.keys()
+    return results
 
 if __name__ == '__main__':
     print(main())
